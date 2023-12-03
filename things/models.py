12f9a6211a8d73593bdac247909c12828d9829bc
Krish Patel <krishp038@gmail.com>
2023-12-03 18:47:45 +0000

    done2 pt2

--- things/models.py
+++ things/models.py
@@ -4,14 +4,4 @@ from django.db import models
 class Thing(models.Model):
     name = models.CharField(max_length = 30, unique = True)
     description = models.CharField(blank = True, max_length = 120)
-    quantity = models.IntegerField(
-        validators = [MinimumLengthValidator(0), MaxValueValidator(100)]
-    )
-    
-     def clean(self):
-        super().clean()
-        if Thing.objects.exclude(pk=self.pk).filter(name=self.name).exists():
-            raise ValidationError({'name': 'Name must be unique'})
-
-    def __str__(self):
-        return f"{self.name} - {self.description} - {self.quantity}"
+    quantity = models.IntegerField()
