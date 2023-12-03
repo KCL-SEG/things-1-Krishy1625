8349c5d54df3ff6931ffe7fae619181c6af18521
Krish Patel <krishp038@gmail.com>
2023-12-03 18:55:13 +0000

    done3 pt2

--- things/models.py
+++ things/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length = 30, unique = True)
-    description = models.CharField(blank = True, max_length = 120)
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True)
+    description = models.TextField(blank=True, max_length=120)
+    quantity = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+
+    def clean(self):
+        super().clean()
+        if Thing.objects.exclude(pk=self.pk).filter(name=self.name).exists():
+            raise ValidationError({'name': 'Name must be unique'})
